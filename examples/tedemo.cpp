6889bdd5b1e9cafbbfabdb2049cf589e80d7e875
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-04-26 18:35:46 +0700

    feat: using more C++ types and cpp23 API

--- examples/tedemo.cpp
+++ examples/tedemo.cpp
@@ -2,11 +2,14 @@
 
 #include "tinyexpr.h"
 #include <iostream>
+#include <print>
+#include <format>
 
 int main(int argc, char* argv[]){
     te_parser tep;
-    const char* c = "sqrt(5^2+7^2+11^2+(8-2)^2)";
-    double r = tep.evaluate(c);
-    std::cout << "The expression:\n\t" << c << "\nevaluates to:\n\t" << r << "\n";
+    std::string c = "sqrt(5^2+7^2+11^2+(8-2)^2)";
+    auto r = tep.evaluate(c);
+    //std::cout << "The expression:\n\t" << c << "\nevaluates to:\n\t" << r << "\n";
+	std::println("{} eval to {}", c,r);
     return EXIT_SUCCESS;
 }
