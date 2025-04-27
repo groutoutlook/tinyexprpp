1f4ebbe1ee58c6d08cdd892cf9a18fe8486085b3
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-04-27 11:12:22 +0700

    chore: elaborate test case.

--- examples/tedemo.cpp
+++ examples/tedemo.cpp
@@ -1,15 +1,27 @@
-// This is mostly used as a CMake project used for code analysis.
-
 #include "tinyexpr.h"
+#include <format>
 #include <iostream>
 #include <print>
-#include <format>
+#include <regex>
 
-int main(int argc, char* argv[]){
-    te_parser tep;
-    std::string c = "sqrt(5^2+7^2+11^2+(8-2)^2)";
-    auto r = tep.evaluate(c);
-    //std::cout << "The expression:\n\t" << c << "\nevaluates to:\n\t" << r << "\n";
-	std::println("{} eval to {}", c,r);
-    return EXIT_SUCCESS;
+int main(int argc, char *argv[])
+{
+	te_parser   tep;
+	std::string c1 = "sqrt(5^2+7^2+11^2+(8-2)^2)";
+	auto        r1 = tep.evaluate(c1);
+	std::println("{} eval to {}", c1, r1);
+
+	te_type x{2}, y{5}, sum0{9};
+	const std::set<te_variable> lookup{
+	    {"x", &x},
+	    {"y", &y},
+	    {"sum0", sum0}};
+	tep.set_variables_and_functions({{"x", &x}, {"y", &y}});
+
+	std::string c2 = "x+y*90";
+	auto        r2 = tep.evaluate(c2);
+	std::println("{} eval to {}", c2, r2);
+
+
+	return EXIT_SUCCESS;
 }
